92d43615a45ced9c139eafb33637214552636dd2
Baatarsuren Munkhdorj <munkhdorj.bs@gmail.com>
2016-10-27 16:57:21 +0900

    es_utils: aggs,search added

--- es_utils.py
+++ es_utils.py
@@ -4,7 +4,74 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch(['http://127.0.0.1:9200'])
 
 def es_query(query):
-    print json.dumps(query)
     res = es.search(index='ca', doc_type='tweets', body=query, request_timeout=999999999)
     return res
 
+def es_search(field, keyword, start, end):
+    query = {
+        "from": 0,
+        "size": 10000,
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "match_phrase": {
+                            field: keyword,
+                        }
+                    },
+                    {
+                        "range": {
+                            "date": {
+                                "gte": start,
+                                "lte": end
+                            }
+                         }
+                    }
+                ]
+            }
+        },
+        "sort": [
+            {
+                "date": { 
+                    "order": "asc" 
+                }
+            },
+            { 
+                "_score": { 
+                    "order": "desc" 
+                }
+            }
+        ]
+    }
+    return es_query(query)
+
+def es_aggs(field, keyword, start, end):
+    query = {
+        "size": 0,
+        "query": {
+            "match_phrase": {
+                field: keyword,
+            }
+        },
+        "aggs": {
+            "date_range": {
+                "filter": {
+                    "range": {
+                        "date": {
+                            "gte": start,
+                            "lte": end
+                        }
+                    }
+                },
+                "aggs": {
+                    "tweet_histogram": {
+                        "date_histogram": {
+                            "field": "date",
+                            "interval": "day"
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return es_query(query)

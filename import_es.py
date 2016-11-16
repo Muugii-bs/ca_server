d1bedb7d1b8f1c70f1bfb6c46aac52e832dfbf38
Baatarsuren Munkhdorj <munkhdorj.bs@gmail.com>
2016-11-16 15:32:59 +0900

    es_import bug fix

--- import_es.py
+++ import_es.py
@@ -35,6 +35,7 @@ def get_data(id, clan, tweet):
     data = {
         'id'                        : id,
         'user_clan_name'            : clan,
+        'created'                   : convert_date(tweet['created_at']),
         'text'                      : tweet['text'],
         'favorite_count'            : tweet['favorite_count'],
         "retweet_count"             : tweet['retweet_count'],
@@ -47,17 +48,17 @@ def get_data(id, clan, tweet):
         "user_screen_name"          : tweet['user']['screen_name'] 
     }
     if 'retweeted_status' in tweet:
-        data['re_st_createddata']                 = convert_date(tweet['retweeted_status']['created_at'])
-        data['re_st_favorite_countdata']          = tweet['retweeted_status']['favorite_count']
-        data['re_st_retweet_countdata']           = tweet['retweeted_status']['retweet_count'] 
-        data['re_st_retweeteddata']               = tweet['retweeted_status']['retweeted']
-        data['re_st_textdata']                    = tweet['retweeted_status']['text']
-        data['re_st_user_createddata']            = convert_date(tweet['retweeted_status']['user']['created_at'])
-        data['re_st_user_descriptiondata']        = tweet['retweeted_status']['user']['description']
-        data['re_st_user_followers_countdata']    = tweet['retweeted_status']['user']['followers_count']
-        data['re_st_user_friends_countdata']      = tweet['retweeted_status']['user']['friends_count']
-        data['re_st_user_namedata']               = tweet['retweeted_status']['user']['name']
-        data['re_st_user_screen_namedata']        = tweet['retweeted_status']['user']['screen_name']
+        data['re_st_created']                 = convert_date(tweet['retweeted_status']['created_at'])
+        data['re_st_favorite_count']          = tweet['retweeted_status']['favorite_count']
+        data['re_st_retweet_count']           = tweet['retweeted_status']['retweet_count'] 
+        data['re_st_retweeted']               = tweet['retweeted_status']['retweeted']
+        data['re_st_text']                    = tweet['retweeted_status']['text']
+        data['re_st_user_created']            = convert_date(tweet['retweeted_status']['user']['created_at'])
+        data['re_st_user_description']        = tweet['retweeted_status']['user']['description']
+        data['re_st_user_followers_count']    = tweet['retweeted_status']['user']['followers_count']
+        data['re_st_user_friends_count']      = tweet['retweeted_status']['user']['friends_count']
+        data['re_st_user_name']               = tweet['retweeted_status']['user']['name']
+        data['re_st_user_screen_name']        = tweet['retweeted_status']['user']['screen_name']
     return data
 
 
